420cb14af18e24e809300f8b55f55b36ab72a0ec
tussakin <131391869+tussakin@users.noreply.github.com>
2024-03-06 12:05:31 +0100

    Test

--- src/components/PeopleReader.js
+++ src/components/PeopleReader.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./PeopleReader.css";
 
+
+
+// Test
 export default function PeopleReader() {
     const [data, setData] = useState(null);
 
